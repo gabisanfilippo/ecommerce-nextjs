f0600a4525d1b4e74633e3d0690fa191fdaa3df2
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-03-07 12:05:49 -0300

    feat: header mock

---
+++ src/mocks/header.ts
@@ -0,0 +1,23 @@
+export const navList = [
+  {
+    name: "Plant pots",
+  },
+  {
+    name: "Ceramics",
+  },
+  {
+    name: "Tables",
+  },
+  {
+    name: "Chairs",
+  },
+  {
+    name: "Crockery",
+  },
+  {
+    name: "Tableware",
+  },
+  {
+    name: "Cutlery",
+  },
+];
