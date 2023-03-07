667d3f3fce187accc637adfab4429f170f78b659
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-03-07 12:04:45 -0300

    feat: addition of header and change in head description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Inter } from 'next/font/google'
 import { HeadFC } from '@components/global/Head';
+import { HeaderHome } from '@components/global/HeaderHome';
 
 export default function Home() {
   return (
     <>
-      <HeadFC completeTitle='' description='E-commerce desenvolvido com NextJs' />
+      <HeadFC completeTitle="" description="E-commerce powered by NextJs" />
+      <HeaderHome />
       <h1>h1</h1>
       <h2>h2</h2>
       <h3>h3</h3>
